a18e12885052b37130b8ebfb1d1bfc6a235cb80c
Oleg Rakhmatulin <oleg.rakhmatulin@gmail.com>
2022-05-23 09:54:10 +0200

    Initial version of ExcelJS add-in that uses pre-generated gRPC-Web serice/protocol files.

---
+++ excel/src/functions/functions.js
@@ -0,0 +1,30 @@
+﻿/* eslint-disable @typescript-eslint/no-unused-vars */
+/* global console setInterval, clearInterval */
+
+const {SayHelloRequest, SayHelloResponse} = require('../client/greeter_pb.js');
+const {GreeterServiceClient} = require('../client/greeter_grpc_web_pb.js');
+
+/**
+ * Calls the GreeterService.sayHello gRPC method.
+ * @customfunction sayHello sayHello
+ * @param {string} name String to send.
+ * @returns Returned String.
+ */
+function sayHello(name) {
+  let target = 'https://services-gateway-v1-rruykyywdq-ew.a.run.app';
+  var greeterService = new GreeterServiceClient(target);
+
+  console.log(name);
+
+  return new Promise(function (resolve, reject) {
+
+    var request = new SayHelloRequest();
+    request.setName(name);
+
+    greeterService.sayHello(request, {},
+      function(err, response) {
+        console.log(err);
+        resolve(response.getMessage());
+    });
+  });
+}
